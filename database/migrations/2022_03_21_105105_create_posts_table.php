f70bec57ba634afb2761fc41eecd698919370fa0
Faye <fayebheevayne@gmail.com>
2022-03-21 20:02:51 +0800

    updated create posts

--- database/migrations/2022_03_21_105105_create_posts_table.php
+++ database/migrations/2022_03_21_105105_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
